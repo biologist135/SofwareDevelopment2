7b758d2f519cb4cfd6c97fde52df8f8b21a4dacc
Benjamin Platt <benjamin.platt@csuglobal.edu>
2026-05-13 16:07:09 -0700

    converted the sum of numbers to the product of numbers

--- SumRecursive/src/recursive/RecursiveCalculation.java
+++ SumRecursive/src/recursive/RecursiveCalculation.java
@@ -5,22 +5,22 @@ import java.util.InputMismatchException;
 
 public class RecursiveCalculation {
 	
-	public static int sumNumbers(int count, Scanner scanner) {
+	public static int productNumbers(int count, Scanner scanner) {
 		int number;
 		
 		if (count <=0) {
-			return 0;
+			return 1;
 		}
 		
 		number = getValidInt(scanner);
-		return number + sumNumbers(count - 1, scanner);
+		return number * productNumbers(count - 1, scanner);
 		}
 		
 	
 	public static int getValidInt(Scanner scanner) {
 		while (true) {
 			try {
-				System.out.println("Please enter a number you would like to add.");
+				System.out.println("Please enter a number you would like to multiply.");
 				return scanner.nextInt();
 			}catch(InputMismatchException e) {
 				System.out.println("That is not a valid input. Please enter a whole number!");
@@ -30,10 +30,10 @@ public class RecursiveCalculation {
 	}
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int value = 7;
-		int result = sumNumbers(value, scanner);
+		int value = 5;
+		int result = productNumbers(value, scanner);
 		
-		System.out.println("The sum of numbers from the " + value + " numbers selected is " + result);
+		System.out.println("The product calculated with the " + value + " numbers selected is " + result +".");
 	}
 
 }
